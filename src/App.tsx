e3c7d520235f69240e6ab72e82f6c51258178599
sldeaals <danielariasaguero@gmail.com>
2024-05-08 18:55:04 -0400

    fix: app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./App.css";
 import "./App.scss";
-import { Fieldset, PasswordInput } from "./components";
+import { Fieldset, PasswordInput, PasswordGenerator } from "./components";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Password Validator Input</p>
+        <p>Password Validator</p>
       </header>
       <div className="App-body">
         <div className="App-body-input">
           <Fieldset>{<PasswordInput validate showCriteria />}</Fieldset>
+        </div>
+        <div className="App-body-input">
+          <Fieldset>{<PasswordGenerator />}</Fieldset>
         </div>
       </div>
       <footer className="App-footer">
