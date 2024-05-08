8d95f6904bfacadde0102f7da27a5e244a31abe8
sldeaals <danielariasaguero@gmail.com>
2024-05-08 18:58:20 -0400

    feat: create password generator component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import BasicTextField from "./BasicTextField";
 import PasswordInput from "./PasswordInput";
 import Fieldset from "./Fieldset";
+import PasswordGenerator from "./PasswordGenerator";
 
-export { BasicTextField, PasswordInput, Fieldset };
+export { BasicTextField, PasswordInput, Fieldset, PasswordGenerator };
