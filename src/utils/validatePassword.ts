97bdf35365b8ac4a771b1469555c5132ded60e42
sldeaals <danielariasaguero@gmail.com>
2024-05-12 20:44:09 -0400

    fix: validate password

--- src/utils/validatePassword.ts
+++ src/utils/validatePassword.ts
@@ -1,48 +1,36 @@
 import { PasswordRules, ValidPassword } from "../types";
 import { RULES } from "../utils";
-import { MAX_LENGTH, RULES_MESSAGES } from "./constants";
+import { RULES_MESSAGES, MIN_LEN, CHAR_COUNT } from "./constants";
 
 const validatePassword = (
   text: string,
   rules: PasswordRules,
 ): ValidPassword => {
-  const {
-    length = MAX_LENGTH,
-    isUpperCase,
-    isLowerCase,
-    hasNumber,
-    hasSymbol,
-  } = rules;
+  const { isUpperCase, isLowerCase, hasNumber, hasSymbol } = rules;
 
   const upperCaseCount: number = (text.match(RULES.upperCase) || []).length;
   const lowerCaseCount: number = (text.match(RULES.lowerCase) || []).length;
-  const numberCount: number = (text.match(RULES.number) || []).length;
-  const symbolCount: number = (text.match(RULES.symbol) || []).length;
-
-  const minLength: number = 8;
-  const maxLength: number = length < 12 ? 16 : length;
+  const numberCount: number = (text.match(RULES.numbers) || []).length;
+  const symbolCount: number = (text.match(RULES.symbols) || []).length;
 
   let message: string = "";
   let valid: boolean = true;
 
   if (text.length > 0) {
-    if (text.length < minLength) {
+    if (text.length < MIN_LEN) {
       message = RULES_MESSAGES.minLength;
       valid = false;
-    } else if (text.length > maxLength) {
-      message = `Password must be between ${minLength} and ${maxLength} characters long`;
-      valid = false;
-    } else if (isLowerCase && lowerCaseCount < 2) {
+    } else if (isLowerCase && lowerCaseCount < CHAR_COUNT) {
       message = RULES_MESSAGES.lowerCase;
       valid = false;
-    } else if (isUpperCase && upperCaseCount < 2) {
+    } else if (isUpperCase && upperCaseCount < CHAR_COUNT) {
       message = RULES_MESSAGES.upperCase;
       valid = false;
-    } else if (hasNumber && numberCount < 2) {
-      message = RULES_MESSAGES.number;
+    } else if (hasNumber && numberCount < CHAR_COUNT) {
+      message = RULES_MESSAGES.numbers;
       valid = false;
-    } else if (hasSymbol && symbolCount < 2) {
-      message = RULES_MESSAGES.symbol;
+    } else if (hasSymbol && symbolCount < CHAR_COUNT) {
+      message = RULES_MESSAGES.symbols;
       valid = false;
     }
   }
