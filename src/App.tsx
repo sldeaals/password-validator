2f893e2ee9242096e18a35898fb9d9671c9d6071
sldeaals <danielariasaguero@gmail.com>
2024-05-13 22:42:55 -0400

    feat: implement language select component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import "./App.css";
 import "./App.scss";
 import { Fieldset, PasswordInput, PasswordGenerator } from "./components";
+import { translations } from "./translations";
+import LanguageSelect from "./components/LanguagueSelect";
+import useLanguage from "./hooks/useLanguage";
 
 function App() {
+  const { translate } = useLanguage();
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Password Validator</h1>
+        <h1>{translate(`passwordValidator`, translations)}</h1>
       </header>
       <div className="App-body">
+        <LanguageSelect />
         <Fieldset>{<PasswordInput validate showCriteria />}</Fieldset>
         <Fieldset>{<PasswordGenerator />}</Fieldset>
       </div>
@@ -20,7 +25,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {translate(`learnReact`, translations)}
         </a>
       </footer>
     </div>
